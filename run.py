82b755ecc84316a9c02785c2130de6937b5a1f3f
Federico Betti <74817555+federicobetti99@users.noreply.github.com>
2022-12-02 09:01:38 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,29 +4,29 @@ import matplotlib.pyplot as plt
 environment = "hole"  # among random_maze, Umaze, hole, cliff
 
 if environment == "random_maze":  # select random maze
-    from mazes.training_utils import *
-    from mazes.learning_algorithms import *
+    from utils.training_utils import *
+    from utils.learning_algorithms import *
     from environments.random_maze import *
     env_id = "RandomMaze-v0"
     gym.envs.register(id=env_id, entry_point=RandomMaze, max_episode_steps=100)
     env = gym.make(env_id)
 elif environment == "cliff":  # select cliff
-    from mazes.training_utils import *
-    from mazes.learning_algorithms import *
+    from utils.training_utils import *
+    from utils.learning_algorithms import *
     from environments.cliff import *
     env_id = "RandomCliff-v0"
     gym.envs.register(id=env_id, entry_point=RandomCliff, max_episode_steps=100)
     env = gym.make(env_id)
 elif environment == "hole":  # select hole environment
-    from mazes.training_utils import *
-    from mazes.learning_algorithms import *
+    from utils.training_utils import *
+    from utils.learning_algorithms import *
     from environments.hole import *
     env_id = "Hole-v0"
     gym.envs.register(id=env_id, entry_point=RandomHole, max_episode_steps=100)
     env = gym.make(env_id)
 elif environment == "umaze":
-    from mazes.training_utils import *
-    from mazes.learning_algorithms import *
+    from utils.training_utils import *
+    from utils.learning_algorithms import *
     from environments.umaze import *
     env_id = "Umaze-v0"
     gym.envs.register(id=env_id, entry_point=UMaze, max_episode_steps=100)
@@ -39,8 +39,9 @@ test_freq = 50
 num_episodes = 10000
 num_avg = 10
 
+stats = {"SCRN": {}, "SPG": {}, "SPG Entropy": {}}
+
 if train:
-    stats = {"SCRN": {}, "SPG": {}, "SPG Entropy": {}}
     for i in range(num_avg):
         print(f"========== TRAINING RUN {i} OUT OF {num_avg} ===========")
         print("********** TRAINING WITH SCRN **********")
@@ -61,20 +62,18 @@ average_stats = {"SCRN": {}, "SPG": {}, "SPG Entropy": {}}
 std_stats = {"SCRN": {}, "SPG": {}, "SPG Entropy": {}}
 
 average_stats["SCRN"] = {key: np.mean([stats["SCRN"][i][key] for i in range(num_avg)], axis=0)
-                         for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
+                                for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
 average_stats["SPG"] = {key: np.mean([stats["SPG"][i][key] for i in range(num_avg)], axis=0)
-                         for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
+                                for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
 average_stats["SPG Entropy"] = {key: np.mean([stats["SPG Entropy"][i][key] for i in range(num_avg)], axis=0)
                                 for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
 std_stats["SCRN"] = {key: np.std([stats["SCRN"][i][key] for i in range(num_avg)], axis=0) / np.sqrt(num_avg)
-                     for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
+                                for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
 std_stats["SPG"] = {key: np.std([stats["SPG"][i][key] for i in range(num_avg)], axis=0) / np.sqrt(num_avg)
-                    for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
-std_stats["SPG Entropy"] = {key: np.std([stats["SPG Entropy"][i][key] for i in range(num_avg)], axis=0) /
-                                 np.sqrt(num_avg)
-                            for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
+                                for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
+std_stats["SPG Entropy"] = {key: np.std([stats["SPG Entropy"][i][key] for i in range(num_avg)], axis=0) / np.sqrt(num_avg)
+                                for key in ["steps", "rewards", "taus", "obj_estimates", "grad_estimates"]}
 
-plt.rcParams.update(plt.rcParamsDefault)
 
 plt.figure()
 QOI_SCRN = average_stats["SCRN"]["steps"]
@@ -116,7 +115,7 @@ plt.xlabel("Number of episodes", fontsize=20)
 plt.ylabel(r"$\frac{J(\theta^{*}) - J(\theta)}{\vert \vert \nabla J(\theta) \vert \vert}$", fontsize=20)
 plt.savefig(f"figures/{environment}/taus.png")
 
-plt.figure(figsize=(8, 5))
+plt.figure()
 QOI_SCRN = average_stats["SCRN"]["rewards"]
 STD_SCRN = std_stats["SCRN"]["rewards"]
 QOI_SPG = average_stats["SPG"]["rewards"]
@@ -136,7 +135,7 @@ plt.xlabel("Number of episodes", fontsize=20)
 plt.ylabel("Reward during training", fontsize=20)
 plt.savefig(f"figures/{environment}/rewards.png")
 
-plt.figure(figsize=(8, 5))
+plt.figure()
 QOI_SCRN = average_stats["SCRN"]["obj_estimates"]
 STD_SCRN = std_stats["SCRN"]["obj_estimates"]
 QOI_SPG = average_stats["SPG"]["obj_estimates"]
@@ -156,7 +155,7 @@ plt.xlabel("Number of episodes", fontsize=20)
 plt.ylabel("Objective during training", fontsize=20)
 plt.savefig(f"figures/{environment}/objectives.png")
 
-plt.figure(figsize=(8, 5))
+plt.figure()
 QOI_SCRN = average_stats["SCRN"]["grad_estimates"]
 STD_SCRN = std_stats["SCRN"]["grad_estimates"]
 QOI_SPG = average_stats["SPG"]["grad_estimates"]
